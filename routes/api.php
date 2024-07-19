586bca811ee6cdd356016423c3105361425d0936
Martin Acosta <m@sis.uy>
2024-07-19 00:51:19 -0300

    add route persona alta

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PersonaController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::get('/', function () {
-    return response()->json(['message' => 'Hello World!']);
-});
+Route::post('/alta', [PersonaController::class, 'alta'])->name('alta');
