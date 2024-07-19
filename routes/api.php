cb8694883432962f1fd61eb85b5587aa6f13599e
Martin Acosta <m@sis.uy>
2024-07-19 16:00:49 -0300

    add: buscar personas routes

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::post('/alta', [PersonaController::class, 'alta'])->name('alta');
     Route::get('/listar', [PersonaController::class, 'listar'])->name('listar');
+    Route::get('/buscar/{id}', [PersonaController::class, 'buscar'])->name('buscar');
 });
