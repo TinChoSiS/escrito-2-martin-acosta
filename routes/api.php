6283cc696a3e2acf4c13ff6cdf87992e9afd356f
Martin Acosta <m@sis.uy>
2024-07-19 16:54:03 -0300

    add: eliminar persona routes

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,5 @@ Route::prefix('v1')->group(function () {
     Route::post('/alta', [PersonaController::class, 'alta'])->name('alta');
     Route::get('/listar', [PersonaController::class, 'listar'])->name('listar');
     Route::get('/buscar/{id}', [PersonaController::class, 'buscar'])->name('buscar');
+    Route::delete('/eliminar/{id}', [PersonaController::class, 'eliminar'])->name('eliminar');
 });
