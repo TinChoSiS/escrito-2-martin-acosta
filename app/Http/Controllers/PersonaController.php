b5a328838853537775fb6dfe04d3c2f98a9f11c8
Martin Acosta <m@sis.uy>
2024-07-19 00:20:31 -0300

    add persona controller

---
+++ app/Http/Controllers/PersonaController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Persona;
+use Illuminate\Http\Request;
+
+class PersonaController extends Controller
+{
+    public function alta(Request $request)
+    {
+        $persona = new Persona();
+        $persona->nombre = $request->nombre;
+        $persona->apellido = $request->apellido;
+        $persona->telefono = $request->telefono;
+        $persona->save();
+        return response()->json([], 201);
+    }
+}
