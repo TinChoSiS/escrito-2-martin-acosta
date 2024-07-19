7a4bd9e467845c154e6fa53237decc9c58e476ac
Martin Acosta <m@sis.uy>
2024-07-19 17:11:30 -0300

    add: modificar persona routes

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,5 @@ Route::prefix('v1')->group(function () {
     Route::post('/alta', [PersonaController::class, 'alta'])->name('alta');
     Route::get('/listar', [PersonaController::class, 'listar'])->name('listar');
     Route::get('/buscar/{id}', [PersonaController::class, 'buscar'])->name('buscar');
+    Route::put('/modificar/{id}', [PersonaController::class, 'modificar'])->name('modificar');
 });
