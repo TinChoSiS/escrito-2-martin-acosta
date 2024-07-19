083e7458fbf3b71a1a081c2cb503c69002924f96
Martin Acosta <m@sis.uy>
2024-07-19 16:28:05 -0300

    add: tests listar con filtros nombre, apellido, telefono

--- tests/Feature/PersonaTest.php
+++ tests/Feature/PersonaTest.php
@@ -20,15 +20,15 @@ class PersonaTest extends TestCase
 
     function test_alta_con_datos()
     {
-        $response = $this->post('/api/alta', [
-            'nombre' => 'Juan',
-            'apellido' => 'Perez',
+        $response = $this->post('/api/v1/alta', [
+            'nombre' => 'ElViejo',
+            'apellido' => 'DeLaBolsa',
             'telefono' => '1234567890',
         ]);
 
         $response->assertJson([
-            'nombre' => 'Juan',
-            'apellido' => 'Perez',
+            'nombre' => 'ElViejo',
+            'apellido' => 'DeLaBolsa',
             'telefono' => '1234567890',
         ]);
 
@@ -39,7 +39,124 @@ class PersonaTest extends TestCase
             'telefono',
             'updated_at',
         ]);
-        
+
         $response->assertStatus(201);
+    }
+
+    function test_listar_sin_filtros()
+    {
+        $response = $this->get('/api/v1/listar');
+
+        $response->assertJsonStructure([
+            'current_page',
+            'data' => [
+                '*' => [
+                    'id',
+                    'nombre',
+                    'apellido',
+                    'telefono',
+                    'updated_at',
+                ],
+            ],
+            'first_page_url',
+            'from',
+            'last_page',
+            'last_page_url',
+            'next_page_url',
+            'path',
+            'per_page',
+            'prev_page_url',
+            'to',
+            'total',
+        ]);
+        $response->assertStatus(200);
+    }
+
+    function test_listar_con_filtros()
+    {
+        $peronsa = [
+            'nombre' => 'ElViejo',
+            'apellido' => 'DeLaBolsa',
+            'telefono' => '6767676767',
+        ];
+
+        $this->post('/api/v1/alta', $peronsa);
+
+        $response = $this->get('/api/v1/listar?nombre=ElViejo&apellido=DeLaBolsa&telefono=6767676767');
+
+        $response->assertJsonStructure([
+            'current_page',
+            'data' => [
+                '*' => [
+                    'id',
+                    'nombre',
+                    'apellido',
+                    'telefono',
+                    'updated_at',
+                ],
+            ],
+            'first_page_url',
+            'from',
+            'last_page',
+            'last_page_url',
+            'next_page_url',
+            'path',
+            'per_page',
+            'prev_page_url',
+            'to',
+            'total',
+        ]);
+
+        $response->assertJson(['data' => [$peronsa]]);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_listar_con_filtro_nombre()
+    {
+        $persona = [
+            'nombre' => 'ElViejo',
+            'apellido' => 'DeLaBolsa',
+            'telefono' => '6767676767',
+        ];
+
+        $this->post('/api/v1/alta', $persona);
+
+        $response = $this->get('/api/v1/listar?nombre=ElVi');
+
+        $response->assertJson(['data' => [$persona]]);
+        $response->assertStatus(200);
+    }
+
+    public function test_listar_con_filtro_apellido()
+    {
+        $persona = [
+            'nombre' => 'ElViejo',
+            'apellido' => 'DeLaBolsa',
+            'telefono' => '6767676767',
+        ];
+
+        $this->post('/api/v1/alta', $persona);
+
+        $response = $this->get('/api/v1/listar?apellido=DeLa');
+
+        $response->assertJson(['data' => [$persona]]);
+        $response->assertStatus(200);
+    }
+
+    public function test_listar_con_filtro_telefono()
+    {
+        $persona = [
+            'nombre' => 'ElViejo',
+            'apellido' => 'DeLaBolsa',
+            'telefono' => '6767676767',
+        ];
+
+        $this->post('/api/v1/alta', $persona);
+
+        $response = $this->get('/api/v1/listar?telefono=6767');
+
+        $response->assertJson(['data' => [$persona]]);
+        $response->assertStatus(200);
     }
 }
